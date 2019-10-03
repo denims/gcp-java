60dc8775e757560fe9069cae6ba1de8df0b95e08
denims <denimallel@gmail.com>
2019-10-03 23:09:39 +0530

    initial commit

---
+++ src/main/java/com/deni/web/gcpjava/GcpJavaApplication.java
@@ -0,0 +1,13 @@
+package com.deni.web.gcpjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcpJavaApplication.class, args);
+    }
+
+}
